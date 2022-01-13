b42249512c2be60a3038ea599e513ebbb5559090
Nick Jenkins <nicholas.jenkins@email.ucr.edu>
2022-01-13 12:30:23 -0800

    add correlation

--- data-cleaning.R
+++ data-cleaning.R
@@ -5,3 +5,7 @@ t.test(iris$Sepal.Length, iris$Sepal.Width)
 ft1 <- lm(iris$Sepal.Length ~ iris$Sepal.Width)
 
 plot(iris$Sepal.Length, iris$Sepal.Width)
+
+corr(iris$Sepal.Length, iris$Sepal.Width)
+
+x <- c(1, 2, 3)
